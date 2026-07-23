ba78710acbe2832bd3341a9d5c7245522947b16a
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-07-23 10:53:25 +0900

    uninstall router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./pages/Home";
-import { Details } from "./pages/details";
-import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<Home />);
