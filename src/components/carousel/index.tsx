5e8104377a9d63d04618c7cb92b4a5a73119d7f3
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-07-23 10:37:38 +0900

    add radius value for carousel to @theme

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -21,7 +21,6 @@ export const Carousel = <T,>({ ref, items, renderItem }: Props<T>) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const totalItems = items.length;
   const angleStep = 360 / totalItems;
-  const radius = 300;
 
   const prev = () => {
     setCurrentIndex((prev) => prev - 1);
@@ -37,11 +36,11 @@ export const Carousel = <T,>({ ref, items, renderItem }: Props<T>) => {
 
   return (
     <div className="flex flex-col">
-      <div className="relative w-80 h-96 flex items-center justify-center perspective-midrange">
+      <div className="relative w-56 h-64 md:w-80 md:h-96 [--carousel-radius:var(--radius-carousel-sm)]  md:[--carousel-radius:var(--radius-carousel-md)] flex items-center justify-center perspective-midrange">
         <div
           className="absolute w-full h-full transition-transform duration-700 transform-3d"
           style={{
-            transform: `rotateX(-20deg) translateZ(-${radius}px) rotateY(${-currentIndex * angleStep}deg)`,
+            transform: `rotateX(-20deg) translateZ(calc(-1 * var(--carousel-radius))) rotateY(${-currentIndex * angleStep}deg)`,
           }}
         >
           {items.map((item, index) => {
@@ -56,7 +55,7 @@ export const Carousel = <T,>({ ref, items, renderItem }: Props<T>) => {
                 key={index}
                 className={`absolute inset-0 backface-visible`}
                 style={{
-                  transform: `rotateY(${itemAngle}deg) translateZ(${radius}px)`,
+                  transform: `rotateY(${itemAngle}deg) translateZ(var(--carousel-radius))`,
                 }}
               >
                 {renderItem(item, isActiveIndex)}
