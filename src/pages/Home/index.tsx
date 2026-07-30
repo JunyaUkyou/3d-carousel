496117d70bb8b5128c3d2d9a15e9e6230a1a513d
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-07-30 09:38:18 +0900

    add position fixed to h1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -56,7 +56,7 @@ export const Home = () => {
     <div className="relative  bg-gray-900 text-white">
       <RainEffect isDisplay={effect === effectTypes.RAIN} />
       <SnowEffect isDisplay={effect === effectTypes.SNOW} />
-      <h1 className="text-2xl font-bold">3D Carousel</h1>
+      <h1 className="text-2xl font-bold fixed left-2 top-2">3D Carousel</h1>
 
       <div className="relative flex flex-col gap-6 items-center justify-center min-h-screen">
         <Carousel
